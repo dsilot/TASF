9cb67d8c772950cc647600711e52d7281f26a019
Dariel Silot <zilot8a@gmail.com>
2023-01-05 14:27:08 -0500

    dockerfile y run actualizado

--- run.sh
+++ run.sh
@@ -3,12 +3,14 @@
 #mysql -uroot -proot ventas < ventas.sql;
 
 start(){
-cd ctrlventas
+cd /ctrlventas
+echo "ctrlventas"
 ./mvnw spring-boot:run
 }
 
 startapi(){
-cd ctrlventas
+cd /ctrlventasapi
+echo "ctrlventasapi"
 ./mvnw spring-boot:run
 }
 
