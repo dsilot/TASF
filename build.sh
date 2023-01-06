7a91d95a5cd9ab810f6432b6de7c5d9844aae892
Dariel Silot <zilot8a@gmail.com>
2023-01-06 09:09:51 -0500

    dockerfile actualizado

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-docker-entrypoint.sh
+/usr/local/bin/docker-entrypoint.sh
+
 chmod +x /ctrlventas/mvnw
 chmod +x /ctrlventasapi/mvnw
 cd /ctrlventas/
-#tar xvf ctrlventas.tar
 ls -al
 ./mvnw package
 
-cd /ctrlventasapi/
-#tar xvf ctrlventasapi.tar
-ls -al
-./mvnw package
+#cd /ctrlventasapi/
+#ls -al
+#./mvnw package
 
 
