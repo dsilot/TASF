12544931330381abc2e03ed2d92875d6bf238694
Dariel Silot <zilot8a@gmail.com>
2023-01-06 01:39:28 -0500

    dockerfile y run actualizado

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 chmod +x /ctrlventas/mvnw
 chmod +x /ctrlventasapi/mvnw
 cd /ctrlventas/
+tar --extract --file ctrlventa.zip
 ls -l
 ./mvnw package
 
 cd /ctrlventasapi/
+tar --extract --file ctrlventasapi.zip
 ls -l
 ./mvnw package
 
